423e03187277cf28a4622f2b21d3b10a3d8c5142
giovanni-iannaccone <iannacconegiovanni444@gmail.com>
2024-09-06 11:00:24 +0200

    Bugfix

--- src/api_data.php
+++ src/api_data.php
@@ -1,28 +1,60 @@
 <?php
 
 class Api {
-    private string $api_key;
+    protected string $api_key;
 
     function __construct(string $api_key) {
         $this->api_key = $api_key;
     }
 
-    public function send_request(string $prompt): array {
-        return [];
+    public function send_request(string $prompt): string {
+        return "";
     }
 }
 
 class ChatGPTApi extends Api {
-    public function send_request(string $prompt): array {
-        //TODO: implement this
-        $link = "";
-        return [];
+    public function send_request(string $prompt): string {
+        $url = 'https://api.openai.com/v1/chat/completions';
+
+        $data = [
+            'model' => 'gpt-4o-mini',
+            'messages' => [
+                [
+                    'role' => 'user', 
+                    'content' => $prompt
+                ]
+            ],
+            'temperature' => 0.7
+        ];
+        
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $this->api_key,
+        ]);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            $answere = 'Errore nella richiesta: ' . curl_error($ch);
+        } else {
+            $response_data = json_decode($response, true);
+            print_r($response_data);
+            $answere = $response_data['choices'][0]['message']['content'];
+        }
+
+        curl_close($ch);
+        return $answere;
     }
 }
 
 class UnsplashApi extends Api {
-    public function send_request(string $prompt): array {
+    public function send_request(string $prompt): string {
         //TODO: implement this
-        return [];
+        return "";
     }
 }
--- src/prompt_utils.php
+++ src/prompt_utils.php
@@ -2,7 +2,8 @@
 
 function craft_prompt($return_types, $number_of_data_to_generate): string {
     $base_prompt = "From now on, I will ask you to generate mock data, and you will respond with a string of data in sequence separated by a comma. Nothing else";
-    $prompt = $base_prompt . "Generate " . $number_of_data_to_generate . " with this structure " . $return_types ;
+    $return_types_string = json_encode($return_types);
 
+    $prompt = $base_prompt . "Generate " . $number_of_data_to_generate . " with this structure " . $return_types_string ;
     return $prompt;
 }
