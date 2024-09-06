2efdee1f8f246e4f53bce73850779ae4af696f7c
giovanni-iannaccone <iannacconegiovanni444@gmail.com>
2024-09-06 11:04:03 +0200

    Fix prompt

--- src/prompt_utils.php
+++ src/prompt_utils.php
@@ -1,7 +1,7 @@
 <?php 
 
 function craft_prompt($return_types, $number_of_data_to_generate): string {
-    $base_prompt = "From now on, I will ask you to generate mock data, and you will respond with a string of data in sequence separated by a comma. Nothing else";
+    $base_prompt = "From now on, I will ask you to generate mock data, and you will respond with a string of data in sequence separated by a comma. Nothing else. ";
     $return_types_string = json_encode($return_types);
 
     $prompt = $base_prompt . "Generate " . $number_of_data_to_generate . " with this structure " . $return_types_string ;
